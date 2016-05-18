b60a9e7f5e32164d3852027349c677f96459496d
Alua.Kinzhebayeva <Alua.Kinzhebayeva@gmail.com>
2016-05-18 16:01:31 +0600

    attachRecipients fix

--- events/recipients/attachRecipients/action.js
+++ events/recipients/attachRecipients/action.js
@@ -9,57 +9,61 @@ const sns = new AWS.SNS();
 let sentRecipientsCount = 0;
 let totalRecipientsToSendCount = 0;
 
-export function respond(event, cb){
-  let msg = JSON.parse(event.Records[0].Sns.Message);
-  if(msg.recipientsCount && msg.campaign && msg.campaign.listIds){
+export function respond(event, cb) {
+  const msg = JSON.parse(event.Records[0].Sns.Message);
+  if (msg.recipientsCount && msg.campaign && msg.campaign.listIds) {
     totalRecipientsToSendCount = msg.recipientsCount;
     const listIds = msg.campaign.listIds;
-    sendCampaignRecipients(listIds, msg, cb).then(()=>{
-      if(!isRecipientsSendingInProgress()){
+    sendCampaignRecipients(listIds, msg, cb).then(() => {
+      if (!isRecipientsSendingInProgress()) {
         return cb(null, `Campaign ${msg.campaign.id} recipients have been successfully sent`);
       }
     }).catch(e => {
       debug(e);
       return cb(e);
     });
-  }else{
+  } else {
     debug('AttachRecipientsService', 'Could not send campaign recipients');
     return cb('Required parameters are missing');
   }
 }
 
-function isRecipientsSendingInProgress(){
+function isRecipientsSendingInProgress() {
   return sentRecipientsCount < totalRecipientsToSendCount;
 }
 
-function sendCampaignRecipients(listIds, msg, cb){
+function sendCampaignRecipients(listIds, msg, cb) {
   let getLists = [];
-  listIds.forEach(function(id){
-    getLists.push(sendNextBatch(id, msg, cb));
+  const recipientParams = {
+    status: 'NORMAL'
+  };
+  listIds.forEach((id) => {
+    getLists.push(sendNextBatch(id, msg, recipientParams, cb));
   });
   return Promise.all(getLists);
 }
 
-function sendNextBatch(id, msg, cb){
-  return getAll(id).then(data => {
-    let messages = data.Items.map(r => {
+function sendNextBatch(id, msg, params, cb) {
+  return getAll(id, params).then(data => {
+    const messages = data.Items.map(r => {
       let message = msg;
       message.recipient = r;
       return publishToSns(message, cb);
     });
-    Promise.all(messages).then().catch(e =>{
+    Promise.all(messages).then().catch(e => {
       debug(e);
     });
-    if(data.LastEvaluatedKey){
-      let params = {
-        LastEvaluatedKey: data.LastEvaluatedKey
+    if (data.LastEvaluatedKey) {
+      const params = {
+        status: 'NORMAL',
+        page: data.LastEvaluatedKey
       };
-      return sendNextBatch(id, params);
-    }else{
+      return sendNextBatch(id, msg, params, cb);
+    } else {
       return Promise.resolve();
     }
   });
-};
+}
 
 function publishToSns(canonicalMessage, cb) {
   return new Promise((resolve, reject) => {
@@ -69,20 +73,19 @@ function publishToSns(canonicalMessage, cb) {
       TopicArn: process.env.PRECOMPILE_EMAIL_TOPIC_ARN
     };
     sns.publish(params, (err, data) => {
-      let email = canonicalMessage.recipient.email;
       if (err) {
         if (err.code === 'Throttling' || err.code === 'InternalFailure') {
           publishToSns(canonicalMessage);
           resolve();
-        }else{
+        } else {
           debug('AttachRecipientsService.publishToSns', 'Error sending message', err);
           reject(err);
         }
       } else {
         sentRecipientsCount++;
         debug('AttachRecipientsService.publishToSns', 'Message sent');
-        if(!isRecipientsSendingInProgress()){
-          return cb(null, `Campaign ${msg.campaign.id} recipients have been successfully sent`);
+        if (!isRecipientsSendingInProgress()) {
+          return cb(null, `Campaign ${canonicalMessage.campaign.id} recipients have been successfully sent`);
         }
         resolve(data);
       }
