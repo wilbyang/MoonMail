d3fc6d8a17c96bdbd62dbc0d6fc7972484d1e5b3
David García <david.garcia@microapps.com>
2016-05-24 17:36:42 +0200

    Email notifications handler function

---
+++ events/reports/emailNotification/action.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import { parse } from 'aws-event-parser';
+import { EmailNotificationService } from '../../lib/email_notification_service';
+import { debug } from '../../lib/index';
+
+export function respond(event, cb) {
+  debug('= emailNotification.action', JSON.stringify(event));
+  const notification = parse(event)[0];
+  const emailNotificationService = new EmailNotificationService(notification);
+  emailNotificationService.process()
+    .then((data) => cb(null, data))
+    .catch((err) => cb(err));
+}
