46dcf00db1729607a263b401ee611e43e8d7ccda
David García <david.garcia@microapps.com>
2016-06-03 11:32:44 +0200

    Delete recipient fix

--- api/recipients/deleteRecipient/handler.js
+++ api/recipients/deleteRecipient/handler.js
@@ -2,7 +2,7 @@
 import * as action from './action';
 
 export default (event, context) => {
-  action.respond(event, function(error, response) {
+  action.respond(event, (error, response) => {
     return context.done(error, response);
   });
 };
