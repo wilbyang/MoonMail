1c985e6347ef772dd472becf91298d8b0201df38
Alua.Kinzhebayeva <Alua.Kinzhebayeva@gmail.com>
2016-06-15 17:50:51 +0200

    subscribe service

---
+++ api/recipients/subscribeRecipient/action.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import { Recipient } from 'moonmail-models';
+import { debug } from '../../lib/logger';
+import base64url from 'base64-url';
+
+export function respond(event, cb) {
+  debug('= subscribeRecipient.action', JSON.stringify(event));
+  if (event.listId && event.recipient && event.recipient.email) {
+    const recipient = event.recipient;
+    recipient.listId = event.listId;
+    recipient.id = base64url.encode(recipient.email);
+    recipient.recipientStatus = recipient.recipientStatus || 'NORMAL';
+    Recipient.save(recipient).then(() => cb(null, recipient))
+    .catch(e => {
+      debug(e);
+      return cb(e);
+    });
+  } else {
+    return cb('No recipient specified');
+  }
+}
