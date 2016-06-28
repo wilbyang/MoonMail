781628cf0efdd3a7b423937eaf55ce1f3f94e1db
Carlos Castellanos Vera <ccvera@ccvera-pc-2.local>
2016-06-28 09:53:21 +0200

    Use s3 metadata for better header names

--- events/lib/import_recipients_service.test.js
+++ events/lib/import_recipients_service.test.js
@@ -6,8 +6,9 @@ import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import awsMock from 'aws-sdk-mock';
 import AWS from 'aws-sdk';
-import fs from 'fs';
 import { ImportRecipientsService } from './import_recipients_service';
+import { Recipient } from 'moonmail-models';
+import * as sinonAsPromised from 'sinon-as-promised';
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
 chai.use(chaiThings);
@@ -15,7 +16,7 @@ chai.use(chaiThings);
 describe('ImportRecipientsService', () => {
   const lambdaContext = {
     functionName: 'lambda-function-name',
-    getRemainingTimeInMillis: () => {}
+    getRemainingTimeInMillis: () => { }
   };
   let contextStub;
   let lambdaClient;
@@ -23,12 +24,13 @@ describe('ImportRecipientsService', () => {
 
   before(() => {
     awsMock.mock('S3', 'getObject', {
-      data: {
-        Body: fs.readFile(require('path').resolve(
-                __dirname,
-                'fixtures/recipients_import.csv'))
-      }
+      Body: `email,first name,last name
+            "em1@examplemail.com","firstName1","lastName1"
+            "em2@examplemail.com","firstName2","lastName2"
+            `,
+      Metadata: '{"first name":"name","last name":"surname"}'
     });
+    sinon.stub(Recipient, 'saveAll').resolves('Ok');
     contextStub = sinon.stub(lambdaContext, 'getRemainingTimeInMillis').returns(100000);
     awsMock.mock('Lambda', 'invoke', 'ok');
     lambdaClient = new AWS.Lambda();
@@ -48,12 +50,33 @@ describe('ImportRecipientsService', () => {
         }
       };
       it('imports recipients', (done) => {
-        const importRecipientsService = new ImportRecipientsService(serviceParams, lambdaClient, contextStub);
+        const importRecipientsService = new ImportRecipientsService(serviceParams, s3Client, lambdaClient, contextStub);
         expect(importRecipientsService).to.have.property('listId', 'listId#1');
         expect(importRecipientsService).to.have.property('fileExt', 'csv');
         expect(importRecipientsService).to.have.property('importOffset', 0);
         expect(importRecipientsService).to.have.deep.property('s3Event', serviceParams.s3Event);
-        done();
+        importRecipientsService.importAll().then(() => {
+          expect(Recipient.saveAll).to.have.been.called;
+          const args = Recipient.saveAll.lastCall.args[0];
+          expect(args.length).to.equals(2);
+          
+          expect(args[0].userId).to.equals('userId');
+          expect(args[0].listId).to.equals('listId#1');
+          expect(args[0].email).to.equals('em1@examplemail.com');
+          expect(args[0].status).to.equals('subscribed');
+          expect(args[0].isConfirmed).to.be.true;
+          expect(args[0].metadata).to.deep.equals({ name: 'firstName1', surname: 'lastName1' });
+          
+
+          expect(args[1].userId).to.equals('userId');
+          expect(args[1].listId).to.equals('listId#1');
+          expect(args[1].email).to.equals('em2@examplemail.com');
+          expect(args[1].status).to.equals('subscribed');
+          expect(args[1].isConfirmed).to.be.true;
+          expect(args[1].metadata).to.deep.equals({ name: 'firstName2', surname: 'lastName2' });
+          
+          done();
+        }).catch((err) => console.log(err, err.stack));
       });
     });
   });
@@ -62,5 +85,6 @@ describe('ImportRecipientsService', () => {
     awsMock.restore('S3');
     awsMock.restore('Lambda');
     contextStub.restore();
+    Recipient.saveAll.restore();
   });
 });
--- events/recipients/importRecipients/action.js
+++ events/recipients/importRecipients/action.js
@@ -1,13 +1,16 @@
 import AWS from 'aws-sdk';
 import { ImportRecipientsService } from '../../lib/import_recipients_service';
 
+const s3 = new AWS.S3({ region: process.env.SERVERLESS_REGION || 'us-east-1' });
+
 export function respond(event, cb, context) {
   const lambda = new AWS.Lambda();
   const params = {
     s3Event: event.Records[0].s3,
     importOffset: event.importOffset
   };
-  const importRecipientsService = new ImportRecipientsService(params, lambda, context);
-  importRecipientsService.importAll().then(status => cb(null, status))
-  .catch(status => cb(JSON.stringify(status)));
+  const importRecipientsService = new ImportRecipientsService(params, s3, lambda, context);
+  importRecipientsService.importAll()
+    .then(status => cb(null, status))
+    .catch(status => cb(JSON.stringify(status)));
 }
