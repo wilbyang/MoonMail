227f33402ccd6ecc790b79ccdf85ec41c6f30baf
Carlos Castellanos Vera <ccvera@ccvera-pc-2.local>
2016-06-28 15:40:30 +0200

    Auto detect csv delimiter and save detected metadata attribute names

--- events/lib/import_recipients_service.test.js
+++ events/lib/import_recipients_service.test.js
@@ -7,7 +7,7 @@ import sinonChai from 'sinon-chai';
 import awsMock from 'aws-sdk-mock';
 import AWS from 'aws-sdk';
 import { ImportRecipientsService } from './import_recipients_service';
-import { Recipient } from 'moonmail-models';
+import { Recipient, List } from 'moonmail-models';
 import * as sinonAsPromised from 'sinon-as-promised';
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
@@ -16,29 +16,29 @@ chai.use(chaiThings);
 describe('ImportRecipientsService', () => {
   const lambdaContext = {
     functionName: 'lambda-function-name',
-    getRemainingTimeInMillis: () => { }
+    getRemainingTimeInMillis: () => { return 100000; }
   };
-  let contextStub;
   let lambdaClient;
   let s3Client;
 
   before(() => {
     awsMock.mock('S3', 'getObject', {
       Body: `email address;first name;last name
-            "em1@examplemail.com";"firstName1";"lastName1"
-            "em2@examplemail.com";"firstName2";"lastName2"
-            `,
+"em1@examplemail.com";"firstName1";"lastName1"
+"em2@examplemail.com";"firstName2";"lastName2"`,
       Metadata: {
-        headers: '{"email address":"email", "first name":"name","last name":"surname"}' 
+        headers: '{"email address":"email", "first name":"name","last name":"surname"}'
       }
     });
+    sinon.stub(List, 'update').resolves('Ok');
+
     sinon.stub(Recipient, 'saveAll').resolves('Ok');
-    contextStub = sinon.stub(lambdaContext, 'getRemainingTimeInMillis').returns(100000);
     awsMock.mock('Lambda', 'invoke', 'ok');
     lambdaClient = new AWS.Lambda();
     s3Client = new AWS.S3();
   });
 
+  // TODO: Add error ocurred case
   describe('#importAll', () => {
     context('when a csv file uploaded', () => {
       const serviceParams = {
@@ -52,7 +52,7 @@ describe('ImportRecipientsService', () => {
         }
       };
       it('imports recipients', (done) => {
-        const importRecipientsService = new ImportRecipientsService(serviceParams, s3Client, lambdaClient, contextStub);
+        const importRecipientsService = new ImportRecipientsService(serviceParams, s3Client, lambdaClient, lambdaContext);
         expect(importRecipientsService).to.have.property('listId', 'listId#1');
         expect(importRecipientsService).to.have.property('fileExt', 'csv');
         expect(importRecipientsService).to.have.property('importOffset', 0);
@@ -61,20 +61,29 @@ describe('ImportRecipientsService', () => {
           expect(Recipient.saveAll).to.have.been.called;
           const args = Recipient.saveAll.lastCall.args[0];
           expect(args.length).to.equals(2);
+
           expect(args[0].userId).to.equals('userId');
           expect(args[0].listId).to.equals('listId#1');
           expect(args[0].email).to.equals('em1@examplemail.com');
           expect(args[0].status).to.equals('subscribed');
           expect(args[0].isConfirmed).to.be.true;
           expect(args[0].metadata).to.deep.equals({ name: 'firstName1', surname: 'lastName1' });
+
           expect(args[1].userId).to.equals('userId');
           expect(args[1].listId).to.equals('listId#1');
           expect(args[1].email).to.equals('em2@examplemail.com');
           expect(args[1].status).to.equals('subscribed');
           expect(args[1].isConfirmed).to.be.true;
           expect(args[1].metadata).to.deep.equals({ name: 'firstName2', surname: 'lastName2' });
+
+          expect(List.update).to.have.been.calledOnce;
+          const updateArgs = List.update.lastCall.args;
+          expect(updateArgs[0]).to.deep.equals({metadataAttributes: ['name', 'surname']});
+          expect(updateArgs[1]).to.equals('userId');
+          expect(updateArgs[2]).to.equals('listId#1');
+          
           done();
-        }).catch((err) => console.log(err, err.stack));
+        });
       });
     });
   });
@@ -82,7 +91,7 @@ describe('ImportRecipientsService', () => {
   after(() => {
     awsMock.restore('S3');
     awsMock.restore('Lambda');
-    contextStub.restore();
     Recipient.saveAll.restore();
+    List.update.restore();
   });
 });
