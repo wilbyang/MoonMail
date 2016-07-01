fd7024aa22d13be3c3a484f83dcbb21878ef6a9a
David García <david.garcia@microapps.com>
2016-07-01 11:44:25 +0200

    Get links api

---
+++ api/links/getLinks/action.js
@@ -0,0 +1,29 @@
+'use strict';
+
+import { Link } from 'moonmail-models';
+import { debug } from '../../lib/logger';
+import decrypt from '../../lib/auth-token-decryptor';
+import { ApiErrors } from '../../lib/errors';
+
+export function respond(event, cb) {
+  debug('= getLinks.action', JSON.stringify(event));
+  decrypt(event.authToken).then((decoded) => {
+    if (event.campaignId) {
+      const options = {};
+      if (event.options) {
+        Object.assign(options, event.options);
+      }
+      Link.allBy('id', event.campaignId, options).then(links => {
+        debug('= getLinks.action', 'Success');
+        return cb(null, links);
+      })
+      .catch(e => {
+        debug('= getLinks.action', 'Error getting links', e);
+        return cb(ApiErrors.response(e));
+      });
+    } else {
+      return cb(ApiErrors.response('No links specified'));
+    }
+  })
+  .catch(err => cb(ApiErrors.response(err), null));
+}
