2c042d0d4edf66a40967222911a66db6d4964cd1
David García <david.garcia@microapps.com>
2016-10-25 15:51:09 +0200

    Export list to CSV

---
+++ api/lists/exportEmailList/action.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import { List } from 'moonmail-models';
+import { debug } from '../../lib/logger';
+import decrypt from '../../lib/auth-token-decryptor';
+import { ApiErrors } from '../../lib/errors';
+import AWS from 'aws-sdk';
+
+AWS.config.region = process.env.SERVERLESS_REGION || 'us-east-1';
+const lambda = new AWS.Lambda();
+
+export function respond(event, cb) {
+  debug('= exportEmailList.action', JSON.stringify(event));
+  decrypt(event.authToken).then((decoded) => {
+    if (event.listId) {
+      List.get(decoded.sub, event.listId)
+        .then(list => checkNoExportPending(list))
+        .then(() => invokeExportLambda(decoded.sub, event.listId))
+        .then(() => cb(null, 'Export started'))
+        .catch(e => cb(ApiErrors.response(e)));
+    } else {
+      return cb(ApiErrors.response('No list specified'));
+    }
+  }).catch(err => cb(ApiErrors.response(err), null));
+}
+
+function checkNoExportPending(list) {
+  return new Promise((resolve, reject) => {
+    const listExports = list.exports || {};
+    const pending = Object.keys(listExports).filter(key => (listExports[key].status && listExports[key].status === 'pending'));
+    if (pending.length > 0) return reject('There are exports in progress');
+    else return resolve(list);
+  });
+}
+
+function invokeExportLambda(userId, listId) {
+  const params = {
+    FunctionName: process.env.EXPORT_FUNCTION_NAME,
+    InvocationType: 'Event',
+    Payload: JSON.stringify({userId, listId})
+  };
+  return lambda.invoke(params).promise();
+}
---
+++ events/lists/exportList/action.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import { debug } from '../../lib/index';
+import ExportListService from '../../lib/export_list_service';
+
+export function respond(event, cb) {
+  debug('= exportList.action', JSON.stringify(event));
+  ExportListService.export(event.listId, event.userId)
+    .then((data) => cb(null, data))
+    .catch((err) => cb(err));
+}
