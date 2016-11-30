71430ac0f44de641dbf585e3c15f537d97e8ef15
David García <david.garcia@microapps.com>
2016-11-30 17:29:15 +0100

    Fix recipient filters

--- api/recipients/listRecipients/action.js
+++ api/recipients/listRecipients/action.js
@@ -1,26 +1,24 @@
-'use strict';
-
 import { Recipient } from 'moonmail-models';
+import omitEmpty from 'omit-empty';
 import { debug } from '../../lib/logger';
 import decrypt from '../../lib/auth-token-decryptor';
 import { ApiErrors } from '../../lib/errors';
-import omitEmpty from 'omit-empty';
 
 export function respond(event, cb) {
   debug('= listRecipients.action', JSON.stringify(event));
   decrypt(event.authToken).then((decoded) => {
     if (event.listId) {
-      let options = {
+      const options = {
         limit: 10
       };
       if (event.options) {
         Object.assign(options, omitEmpty(event.options));
       }
-      const filters = {};
-      if (!!event.email) filters.email = {bw: event.email};
-      if (!!event.status) filters.status = {eq: event.status};
-      Object.assign(options, omitEmpty({filters}));
-      Recipient.allBy('listId', event.listId, options).then(recipients => {
+      let servicePromise;
+      if (!!event.email) servicePromise = Recipient.emailBeginsWith(event.listId, event.email, options);
+      else if (!!event.status) servicePromise = Recipient.allByStatus(event.listId, event.status, options);
+      else servicePromise = Recipient.allBy('listId', event.listId, options);
+      servicePromise.then(recipients => {
         debug('= listRecipients.action', 'Success');
         return cb(null, recipients);
       })
