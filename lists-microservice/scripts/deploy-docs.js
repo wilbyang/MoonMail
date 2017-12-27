f497db36f997f069d8ca6af7f11e7bf20b3e49da
David García <david.garcia@microapps.com>
2017-12-27 12:48:14 +0100

    Docs generation script; Custom domain configuration

--- lists-microservice/scripts/deploy-docs.js
+++ lists-microservice/scripts/deploy-docs.js
@@ -23,6 +23,7 @@ const params = {
   exportType: 'swagger',
   restApiId: program.apiId,
   stageName: program.stage,
+<<<<<<< HEAD
   accepts: 'application/yaml'
 };
 
@@ -34,6 +35,29 @@ function replaceTitle(yamlBody, newTitle) {
 function generateDocs(swaggerString) {
   const swaggerFilePath = '/tmp/moonmail-api-swagger.yml';
   fs.writeFileSync(swaggerFilePath, swaggerString);
+=======
+  accepts: 'application/json'
+};
+
+function replaceTitle(swaggerJson, newTitle) {
+  const newBody = Object.assign({}, swaggerJson);
+  newBody.info.title = newTitle;
+  return newBody;
+}
+
+function shiftBasePath(swaggerJson) {
+  const prefix = swaggerJson.basePath;
+  const paths = Object.keys(swaggerJson.paths).reduce((newPaths, currentPath) => {
+    const newPath = `${prefix}${currentPath}`;
+    return Object.assign({}, newPaths, { [newPath]: swaggerJson.paths[currentPath] });
+  }, {});
+  return Object.assign({}, swaggerJson, { paths, basePath: '' });
+}
+
+function generateDocs(swaggerJson) {
+  const swaggerFilePath = '/tmp/moonmail-api-swagger.json';
+  fs.writeFileSync(swaggerFilePath, JSON.stringify(swaggerJson));
+>>>>>>> Docs generation script; Custom domain configuration
   const options = {
     specFile: swaggerFilePath,
     silent: true,
@@ -43,6 +67,11 @@ function generateDocs(swaggerString) {
 }
 
 apigateway.getExport(params).promise()
+<<<<<<< HEAD
   .then(data => replaceTitle(data.body, 'MoonMail API'))
+=======
+  .then(data => replaceTitle(JSON.parse(data.body), 'MoonMail API'))
+  .then(swaggerBody => shiftBasePath(swaggerBody))
+>>>>>>> Docs generation script; Custom domain configuration
   .then(swaggerBody => generateDocs(swaggerBody))
   .catch(console.log);
