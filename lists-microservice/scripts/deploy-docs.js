80184ed26afe2a048560fce0493b1a50154e17ee
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-01-26 15:11:09 +0300

    Add faker types to generate valid ids, urls and emails for docs

--- lists-microservice/scripts/deploy-docs.js
+++ lists-microservice/scripts/deploy-docs.js
@@ -6,7 +6,23 @@ const fs = require('fs');
 const spectacle = require('spectacle-docs');
 const jsf = require('json-schema-faker');
 const Promise = require('bluebird');
-const traverse = require('traverse');
+const schemaTraverse = require('json-schema-traverse');
+
+jsf.format('cuid', require('cuid'));
+jsf.option({alwaysFakeOptionals: true});
+jsf.extend('chance', function() {
+  const Chance = require('chance');
+  const chance = new Chance();
+  chance.mixin({
+    'metadata': () => ({
+      name: chance.first(),
+      surname: chance.last(),
+      countryCode: chance.country(),
+      foo: 'bar'
+    })
+  });
+  return chance;
+});
 
 program
   .usage('[options]')
@@ -17,7 +33,7 @@ program
   .option('-i, --api-id [apiId]', 'API Gateway ID')
   .parse(process.argv);
 
-const credentials = new aws.SharedIniFileCredentials({ profile: program.profile });
+const credentials = new aws.SharedIniFileCredentials({profile: program.profile});
 aws.config.credentials = credentials;
 aws.config.region = program.region;
 const apigateway = new aws.APIGateway();
@@ -39,43 +55,59 @@ function shiftBasePath(swaggerJson) {
   const prefix = swaggerJson.basePath;
   const paths = Object.keys(swaggerJson.paths).reduce((newPaths, currentPath) => {
     const newPath = `${prefix}${currentPath}`;
-    return Object.assign({}, newPaths, { [newPath]: swaggerJson.paths[currentPath] });
+    return Object.assign({}, newPaths, {[newPath]: swaggerJson.paths[currentPath]});
   }, {});
-  return Object.assign({}, swaggerJson, { paths, basePath: '' });
+  return Object.assign({}, swaggerJson, {paths, basePath: ''});
+}
+
+function addFakerTypes(swaggerJson) {
+  const json = Object.assign({}, swaggerJson);
+  schemaTraverse(json, (schema, pointer, root, parentPointer, parentKey, parentSchema, key) => {
+    if (!key) return;
+    if (key.match(/id$/i)) {
+      schema.format = 'cuid';
+      parentSchema[key] = schema;
+    }
+    if (key.match(/url$/i)) {
+      schema.chance = 'url';
+      parentSchema[key] = schema;
+    }
+    if (key === 'metadata') {
+      schema.chance = 'metadata';
+      parentSchema[key] = schema;
+    }
+    if (key === 'email') {
+      schema.chance = 'email';
+      parentSchema[key] = schema;
+    }
+  });
+  return json;
 }
 
 function addExamples(swaggerJson) {
   const definitions = swaggerJson.definitions;
   const definitionEntries = Object.entries(definitions);
-  const metadataSample = { name: 'Mike', countryCode: 'ES', whatever: 'you want' };
-  jsf.option({ alwaysFakeOptionals: true });
   return Promise.map(definitionEntries, ([key, schema]) => {
-    return jsf.resolve(Object.assign({}, schema, { definitions }))
-      .then(sample => {
-        traverse(sample).forEach((x) => {
-          if (x.hasOwnProperty('metadata')) x.metadata = metadataSample;
-        });
-        return sample;
-      })
-      .then(sample => ({ [key]: sample }));
+    const s = Object.assign({}, schema, {definitions});
+    return jsf.resolve(s).then(sample => ({[key]: sample}));
   })
-  .then(samples => samples.reduce((total, el) => Object.assign(total, el), {}))
-  .then((examples) => {
-    return definitionEntries.reduce((total, [key, schema]) => {
-      total[key] = Object.assign({}, schema, { example: examples[key] });
-      return total;
-    }, {});
-  })
-  .then(defs => Object.assign({}, swaggerJson, { definitions: defs }));
+    .then(samples => samples.reduce((total, el) => Object.assign(total, el), {}))
+    .then((examples) => {
+      return definitionEntries.reduce((total, [key, schema]) => {
+        total[key] = Object.assign({}, schema, {example: examples[key]});
+        return total;
+      }, {});
+    })
+    .then(defs => Object.assign({}, swaggerJson, {definitions: defs}));
 }
 
 function generateDocs(swaggerJson) {
-  const swaggerFilePath = '/tmp/moonmail-api-swagger.json';
+  const swaggerFilePath = './tmp/moonmail-api-swagger.json';
   fs.writeFileSync(swaggerFilePath, JSON.stringify(swaggerJson));
   const options = {
     specFile: swaggerFilePath,
     silent: true,
-    targetDir: '/tmp/moonmail-docs/'
+    targetDir: './tmp/moonmail-docs/'
   };
   spectacle(options);
 }
@@ -83,6 +115,7 @@ function generateDocs(swaggerJson) {
 apigateway.getExport(params).promise()
   .then(data => replaceTitle(JSON.parse(data.body), 'MoonMail API'))
   .then(swaggerBody => shiftBasePath(swaggerBody))
+  .then(swaggerBody => addFakerTypes(swaggerBody))
   .then(swaggerBody => addExamples(swaggerBody))
   .then(swaggerBody => generateDocs(swaggerBody))
   .catch(console.log);
