eae22c3db384a80fefac06ccf1d9a087d59f8a58
David García <david.garcia@microapps.com>
2018-03-20 11:12:39 +0100

    Email opens validator

---
+++ emails-microservice/src/notifications/EmailOpen.js
@@ -0,0 +1,24 @@
+import Joi from 'joi';
+
+const clickSchema = Joi.object({
+  campaignId: Joi.string().required(),
+  listId: Joi.string().required(),
+  linkId: Joi.string().required(),
+  recipientId: Joi.string().required(),
+  userId: Joi.string().required(),
+  segmentId: Joi.string(),
+  httpHeaders: Joi.object()
+});
+
+const isValid = (event) => {
+  try {
+    const result = Joi.validate(event, clickSchema, { allowUnknown: true });
+    return !result.error;
+  } catch (err) {
+    return false;
+  }
+};
+
+export default {
+  isValid
+};
---
+++ emails-microservice/src/notifications/EmailOpen.test.js
@@ -0,0 +1,36 @@
+import { expect } from 'chai';
+import EmailOpen from './EmailOpen';
+
+describe('EmailOpen', () => {
+  describe('.isValid()', () => {
+    const campaignId = 'campaign-id';
+    const linkId = 'link-id';
+    const listId = 'list-id';
+    const recipientId = 'recipient-id';
+    const userId = 'user-id';
+    const segmentId = 'segment-id';
+    const httpHeaders = { Host: 'localhost', 'User-Agent': 'Firefox' };
+
+    context('when the notification is valid', () => {
+      it('returns true', () => {
+        const testCases = [
+          { campaignId, listId, linkId, recipientId, userId, segmentId, httpHeaders },
+          { campaignId, listId, linkId, recipientId, userId, httpHeaders },
+          { campaignId, listId, linkId, recipientId, userId }
+        ];
+        testCases.forEach(testCase => expect(EmailOpen.isValid(testCase)).to.be.true);
+      });
+    });
+
+    context('when the notification is not valid', () => {
+      it('returns false', () => {
+        const testCases = [
+          { linkId, listId, recipientId, userId, segmentId, httpHeaders },
+          { campaignId, listId, recipientId, userId, httpHeaders },
+          { campaignId, linkId, recipientId, userId }
+        ];
+        testCases.forEach(testCase => expect(EmailOpen.isValid(testCase)).to.be.false);
+      });
+    });
+  });
+});
