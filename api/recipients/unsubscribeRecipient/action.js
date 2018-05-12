dd88a07a10109dfac546aa564267705cb7b4543c
JefersonS <jefersonebs@gmail.com>
2018-05-12 16:01:47 -0300

    Fixing recipients to send info to Webhooks;
    Adding recipient params to kinesis put;
    
    Fixing params to be sent to kinesis function;

--- api/recipients/unsubscribeRecipient/action.js
+++ api/recipients/unsubscribeRecipient/action.js
@@ -2,56 +2,110 @@ import * as url from 'url';
 import moment from 'moment';
 import omitEmpty from 'omit-empty';
 import base64url from 'base64-url';
-import { SNS } from 'aws-sdk';
+import { SNS, Kinesis } from 'aws-sdk';
 import { Recipient, List } from 'moonmail-models';
 import { debug } from '../../lib/logger';
 import { ApiErrors } from '../../lib/errors';
 
 const sns = new SNS({ region: process.env.SERVERLESS_REGION });
+const kinesis = new Kinesis({ region: process.env.SERVERLESS_REGION });
 
 export function respond(event, cb) {
-  debug('= unsubscribeRecipient.action', JSON.stringify(event));
-  if (event.listId && event.recipientId) {
-    const recipient = {
-      status: Recipient.statuses.unsubscribed,
-      unsubscribedAt: moment().unix(),
-      unsubscribedCampaignId: event.campaignId
-    };
-    Recipient.update(omitEmpty(recipient), event.listId, event.recipientId).then((newRecipient) => {
-      debug('= unsubscribeRecipient.action', 'Success');
-      const recipientParams = {
-        campaignId: event.campaignId,
-        userId: newRecipient.userId,
-        listId: event.listId
-      };
-      const params = {
-        Message: JSON.stringify(recipientParams),
-        TopicArn: process.env.UNSUBSCRIBED_RECIPIENT_TOPIC_ARN
-      };
-      sns.publish(params, (err, data) => {
-        if (err) {
-          debug('= unsubscribeRecipient.action', 'Error publishing to SNS', JSON.stringify(err));
-          return cb(ApiErrors.response(err));
-        } else {
-          debug('= unsubscribeRecipient.action', 'Successfully published to SNS');
-          List.get(newRecipient.userId, event.listId).then((list) => {
-            const callbackUrl = buildRedirectCallback(process.env.UNSUBSCRIBED_CALLBACK_URL, list, event.recipientId, event.campaignId);
-            return cb(null, { url: callbackUrl });
-          }).catch((e) => {
-            const callbackUrl = process.env.UNSUBSCRIBED_CALLBACK_URL;
-            return cb(null, { url: callbackUrl });
-          });
+  debug('= unsubscribeRecipient.respond', JSON.stringify(event));
+  checkEvent(event)
+    .then(updateRecipient)
+    .then(createSNSParams)
+    .then(publishToSNS)
+    .then(buildURL)
+    .then(publishToKinesis)
+    .then(({ callbackUrl }) => cb(null, { url: callbackUrl }))
+    .catch(e => handleErrors(e, cb))
+}
 
-        }
-      });
-    })
-      .catch((e) => {
-        debug('= unsubscribeRecipient.action', e);
-        return cb(e);
-      });
+const checkEvent = async (event) => {
+  debug('= unsubscribeRecipient.checkEvent', JSON.stringify(event));
+  if (event.listId && event.recipientId) {
+    return {
+      event, recipient: {
+        status: Recipient.statuses.unsubscribed,
+        unsubscribedAt: moment().unix(),
+        unsubscribedCampaignId: event.campaignId
+      }
+    };
   } else {
+    throw 'NO-RECIPIENT';
+  }
+}
+
+const updateRecipient = async ({ event, recipient }) => {
+  debug('= unsubscribeRecipient.updateRecipient', JSON.stringify(recipient));
+  const newRecipient = await Recipient.update(omitEmpty(recipient), event.listId, event.recipientId)
+  return { event, newRecipient }
+}
+
+const createSNSParams = async ({ event, newRecipient }) => {
+  debug('= unsubscribeRecipient.createSNSParams', JSON.stringify(newRecipient));
+  const recipientParams = {
+    campaignId: event.campaignId,
+    userId: newRecipient.userId,
+    listId: event.listId
+  };
+  return {
+    event, newRecipient, params: {
+      Message: JSON.stringify(recipientParams),
+      TopicArn: process.env.UNSUBSCRIBED_RECIPIENT_TOPIC_ARN
+    }
+  };
+}
+
+const publishToSNS = ({ event, newRecipient, params }) => {
+  return new Promise((resolve, reject) => {
+    debug('= unsubscribeRecipient.publishToSNS', JSON.stringify(newRecipient), JSON.stringify(params));
+    sns.publish(params, (err, data) => {
+      if (err) {
+        debug('= unsubscribeRecipient.publishToSNS', 'Error publishing to SNS', JSON.stringify(err));
+        reject('SNS-ERROR')
+      } else {
+        debug('= unsubscribeRecipient.publishToSNS', 'Successfully published to SNS');
+        resolve({ event, newRecipient })
+      }
+    });
+  })
+}
+
+const buildURL = ({ event, newRecipient }) => {
+  return new Promise((resolve, reject) => {
+    debug('= unsubscribeRecipient.buildURL', JSON.stringify(newRecipient));
+    List.get(newRecipient.userId, event.listId).then((list) => {
+      const callbackUrl = buildRedirectCallback(process.env.UNSUBSCRIBED_CALLBACK_URL, list, event.recipientId, event.campaignId);
+      resolve({ event, newRecipient, callbackUrl })
+    }).catch((e) => {
+      const callbackUrl = process.env.UNSUBSCRIBED_CALLBACK_URL;
+      resolve({ event, newRecipient, callbackUrl });
+    });
+  });
+}
+
+const publishToKinesis = async ({ event, newRecipient, callbackUrl }) => {
+  debug('= unsubscribeRecipient.publishToKinesis', JSON.stringify(event), JSON.stringify(callbackUrl), JSON.stringify(newRecipient));
+  const kinesisEvent = { type: `list.${Recipient.statuses.unsubscribed}`, payload: { event: Recipient.statuses.unsubscribed, item: 'list', itemId: event.listId, recipient: { id: newRecipient.id, email: newRecipient.email, listId: newRecipient.listId } } }
+  const kinesisParams = { Data: JSON.stringify(kinesisEvent), PartitionKey: kinesisEvent.type, StreamName: process.env.EVENTS_ROUTER_STREAM_NAME }
+  await kinesis.putRecord(kinesisParams).promise()
+  return { event, callbackUrl };
+}
+
+const handleErrors = async (e, cb) => {
+  if (e == 'NO-RECIPIENT') {
+    debug('= unsubscribeRecipient.action.NO-RECIPIENT', e);
     return cb('No recipient specified');
   }
+  if (e == 'SNS-ERROR') {
+    debug('= unsubscribeRecipient.action.SNS-ERROR', e);
+    return cb(ApiErrors.response(err));
+  }
+
+  debug('= unsubscribeRecipient.action.error', e);
+  return cb(e);
 }
 
 function buildRedirectCallback(baseUrl, list, recipientId, campaignId) {
